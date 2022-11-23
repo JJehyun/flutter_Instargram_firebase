10fc197c79e00c1d5523907bc7e40c5d2270828f
JJehyun <povt99@gmail.com>
2022-11-23 22:50:16 +0900

    server_get_data|| json to List

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import './style.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 void main() {
   runApp(
       MaterialApp(
@@ -7,7 +10,7 @@ void main() {
           home: MyAPP())
   );
 }
-
+//서버에서 데이터 가져오기!!!!
 class MyAPP extends StatefulWidget {
   const MyAPP({Key? key}) : super(key: key);
 
@@ -18,6 +21,18 @@ class MyAPP extends StatefulWidget {
 class _MyAPPState extends State<MyAPP> {
   var tab = 0; //탭 정보 저장
   
+//데이터를 받아오는 함수 데이터를 받아오기 전까지 기다라는 뜻
+  getDate() async{
+    var result = await http.get( Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result2 = jsonDecode(result.body);
+  }
+//앱이 처음 로드되었을때 시작되는 것
+  @override
+  void initState(){
+    super.initState();
+    getDate();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +42,7 @@ class _MyAPPState extends State<MyAPP> {
           IconButton(onPressed: (){}, icon: Icon(Icons.add_box_outlined),iconSize: 30,)
         ],
       ),
-      body: [Text("홈"),Text("샵페이지")][tab],
+      body: [Home(),Text("샵페이지")][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -44,3 +59,44 @@ class _MyAPPState extends State<MyAPP> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+//커스텀 위젯
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(itemCount: 3, itemBuilder: (c, i){
+      return Column(
+        children: [
+          Image.network('https://codingapple1.github.io/kona.jpg'),
+          Container(
+            constraints: BoxConstraints(maxWidth: 600),
+            padding: EdgeInsets.all(20),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('좋아요 100'),
+                Text('글쓴이'),
+                Text('글내용'),
+              ],
+            ),
+          )
+        ],
+      );
+    });
+
+  }
+}
+
