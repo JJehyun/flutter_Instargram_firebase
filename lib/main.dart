1bce9a130b3f66ddf43a49c2610e11ad52328e8a
JJehyun <povt99@gmail.com>
2022-11-23 22:26:04 +0900

    Navigator_bottom_Bar

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,16 @@ void main() {
   );
 }
 
-class MyAPP extends StatelessWidget {
+class MyAPP extends StatefulWidget {
   const MyAPP({Key? key}) : super(key: key);
 
+  @override
+  State<MyAPP> createState() => _MyAPPState();
+}
+
+class _MyAPPState extends State<MyAPP> {
+  var tab = 0; //탭 정보 저장
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +27,20 @@ class MyAPP extends StatelessWidget {
           IconButton(onPressed: (){}, icon: Icon(Icons.add_box_outlined),iconSize: 30,)
         ],
       ),
-      body: TextButton(onPressed: (){},child:Text("버튼"))
+      body: [Text("홈"),Text("샵페이지")][tab],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (index){
+          setState(() {
+            tab = index;
+          });
+        },//
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined),label: '샵') //
+        ],
+      )
     );
   }
 }
